7e9756e8e65bddf695109e38bcd7f39c187ab605
Mostafa Sadidi <mosisadidi2018@gmail.com>
2025-03-16 18:22:30 +0330

    Seems Completed.

---
+++ Answers/40330112072/MainMenu.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainMenu {
+    public void menu() {
+        System.out.println("1. Player VS Player");
+        System.out.println("2. Player VS AI");
+        System.out.println("3. Exit.");
+        System.out.print("Select an option: ");
+    }
+}
